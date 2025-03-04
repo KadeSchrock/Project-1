a2e7f23904709d112842429fb72080ec6e866ae5
Solomon Preston <chezman9001@gmail.com>
2025-03-04 13:18:50 -0600

    Update Shopping cart Visual
    Implemented more functionality to the shopping cart, allowing user to add and remove items. User can also increase quantity of items. Still need to make the cart transfer from page to page.

--- Implementation/Code/shopping-cart.js
+++ Implementation/Code/shopping-cart.js
@@ -16,12 +16,95 @@
 //     </div>
 // </div>
 // </li>
-function addItem() {
-    var test = "$499.99"
+var productsList = [{
+    id: 1,
+    name: "BlazeBoard MKI",
+    price: 499.99,
+    image: "snowboard-design-one.jpg",
+    stock: 32
+}, {
+    id: 2,
+    name: "BlazeBoard MKII",
+    price: 499.99,
+    image: "snowboard-design-two.jpg",
+    stock: 20
+}, {
+    id: 3,
+    name: "BlazeBoard MKIII",
+    price: 499.99,
+    image: "snowboard-design-three.jpg",
+    stock: 36
+}];
+var cart = { total: 0 }
+
+
+function addItem(product) {
+    var cartTotal = document.getElementById("cart-total");
     var element = document.getElementById("cart-nav");
-    var newElement = `<li><div class="col s12 m7"><div class="card horizontal"><div class="card-stacked"><div class="card-content"><div class="card-image"><img src="snowboarder-page-background.PNG"></div><p>Snowboard item test</p><p>${test}</p></div><div class="card-action"><a href="#">This is a link</a></div></div></div></div></li>`
-    element.insertAdjacentHTML('afterend', newElement);
+    const productInCart = document.getElementById(`product-${product.id}`);
+    var quantity = document.getElementById(`quantity-${product.id}`);
+    if (productInCart) {
+        // adjust quantity
+        quantity.value = quantity.value * 1 + 1;
+        cart[product.id].quantity += 1;
+        // add to total
+        cart.total += product.price;
+        // display total
+        cartTotal.innerHTML = "Total: $" + cart.total;
+    } else {
+        // create item card
+        var newElement = `<li id="product-${product.id}"><div class="col s12 m7"><div class="card horizontal center-align"><div class="card-stacked"><div class="card-content"><div class="card-image"><img src="${product.image}" id="center"></div><div id="card-text-content"><p id="product-name">${product.name}</p><p id="price">$${product.price}</p></div><label for="quantity-${product.id}">Quantity:</label>&ensp;<input type="number" name="quantity-${product.id}" id="quantity-${product.id}" value=1 min=1 max=${product.stock} onChange="updateItem(productsList[${product.id - 1}])">&ensp;<button onclick="removeItem(productsList[${product.id - 1}])">Remove</button></div></div></div></div></li>`;
+        // display item card
+        element.insertAdjacentHTML('afterend', newElement);
+        // add item to cart
+        cart[product.id] = {
+            name: product.name,
+            price: product.price,
+            quantity: 1
+        };
+        // adjust total
+        cart.total += product.price;
+        // display total
+        cartTotal.innerHTML = "Total: $" + cart.total;
+    }
 }
-function removeItem() {
-    console.log('Item removed!')
+
+
+function removeItem(product) {
+    var cartTotal = document.getElementById("cart-total");
+    // remove item card
+    const element = document.getElementById(`product-${product.id}`);
+    element.remove();
+    // adjust price
+    var newPrice = product.price * cart[product.id].quantity;
+    var roundedPrice = Math.round(newPrice * 100) / 100;
+    cart.total -= roundedPrice;
+    // remove item from cart
+    delete cart[product.id];
+    // display total
+    cartTotal.innerHTML = "Total: " + cart.total;
+
+}
+
+function updateItem(product) {
+    var cartTotal = document.getElementById("cart-total");
+    const quantity = document.getElementById(`quantity-${product.id}`).value*1;
+
+    // if product is more than quantity
+    if(cart[product.id].quantity > quantity) {
+        var newPrice = product.price*(cart[product.id].quantity - quantity)
+        // reduce total
+        cart.total -= newPrice;
+        cart.total = Math.round(cart.total * 100) / 100
+    }else {
+        var newPrice = product.price*(quantity - cart[product.id].quantity);
+        // else increase total
+        cart.total += newPrice;
+        cart.total = Math.round(cart.total * 100) / 100
+    }
+    // update product quantity
+    cart[product.id].quantity = quantity;
+
+    // display total
+    cartTotal.innerHTML = "Total: " + cart.total;
 }
